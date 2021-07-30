c1e63d5d5d1673e63503d0abb269de6499faaeb8
yexinyu <yexinyu.ye@gmail.com>
2021-07-30 10:25:49 +0800

    dirs change

--- chap05/01_blackjack/utils.py
+++ chap05/01_blackjack/utils.py
@@ -24,5 +24,103 @@ def set_dict(target_dict, value, *args):
 
 
 def get_dict(target_dict, *args):
-    return target_dict.get(str_key(*args),0)
+    return target_dict.get(str_key(*args), 0)
+
+
+def greedy_pi(A, s, Q, a):
+    max_q, a_max_q = -float('inf'), []
+    for a_opt in A:
+        q = get_dict(Q, s, a_opt)
+        if q > max_q:
+            max_q = q
+            a_max_q = [a_opt]
+        elif q == max_q:
+            a_max_q.append(a_opt)
+    n = len(a_max_q)
+    if n == 0:
+        return 0.0
+    return 1.0 / n if a in a_max_q else 0.0
+
+
+def greedy_policy(A, s, Q):
+    max_q, a_max_q = -float('inf'), []
+    for a_opt in A:
+        q = get_dict(Q, s, a_opt)
+        if q > max_q:
+            max_q = q
+            a_max_q = [a_opt]
+        elif q == max_q:
+            a_max_q.append(a_opt)
+    return random.choice(a_max_q)
+
+
+def epsilon_greedy_pi(A, s, Q, a, epsilon=0.1):
+    m = len(A)
+    greedy_p = greedy_pi(A, s, Q, a)
+    if greedy_p == 0:
+        return epsilon / m
+    n = int(1.0 / greedy_p)
+    return (1 - epsilon) * greedy_p + epsilon / m
+
+
+def epsilon_greedy_policy(A, s, Q, epsilon, show_random_num = False):
+    pis = []
+    m = len(A)
+    for i in range(m):
+        pis.append(epsilon_greedy_pi(A, s, Q, A[i], epsilon))
+    rand_value = random.random()
+    for i in range(m):
+        if show_random_num:
+            print("随机数:{:.2f}, 拟减去概率{}".format(rand_value, pis[i]))
+        rand_value -= pis[i]
+        if rand_value < 0:
+            return A[i]
+
+
+def draw_value(value_dict, useable_ace = True, is_q_dict = False, A = None):
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    x = np.arange(1, 11, 1)
+    y = np.arange(12, 22, 1)
+    X, Y = np.meshgrid(x, y)
+    row, col = X.shape
+    Z = np.zeros((row, col))
+    if is_q_dict:
+        n = len(A)
+    for i in range(row):
+        for j in range(col):
+            state_name = str(X[i, j])+"_"+str(Y[i, j])+"_"+str(useable_ace)
+            if not is_q_dict:
+                Z[i, j] = get_dict(value_dict, state_name)
+            else:
+                assert(A is not None)
+                for a in A:
+                    new_state_name = state_name + "_" + str(a)
+                    q = get_dict(value_dict, new_state_name)
+                    if q>= Z[i,j]:
+                        Z[i, j] = q
+    ax.plot_surface(X, Y, Z, rstride=1, cstride=1, color="lightgray")
+    plt.show()
+
+
+def draw_policy(policy, A, Q, epsilon, useable_ace = False):
+    def value_of(a):
+        if a == A[0]:
+            return 0
+        else:
+            return 1
+
+    rows, cols = 11, 10
+    useable_ace = bool(useable_ace)
+    Z = np.zeros((rows, cols))
+    dealer_first_card = np.arange(1,12)
+    player_points = np.arange(12,22)
+    for i in range(11, 22):
+        for j in range(1, 11):
+            s = j, i, useable_ace
+            s= str_key(s)
+            a = policy(A, s, Q, epsilon)
+            Z[i-11, j-1] = value_of(a)
+
+    plt.imshow(Z, cmap=plt.cm.cool, interpolation=None, origin="lower", extent=[0.5, 11.5, 10.5, 21.5])
 
